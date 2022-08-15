22a763209452e095b69a76db08ad0b611915b165
Dominik Nakamura <dnaka91@gmail.com>
2022-08-16 02:53:35 +0900

    Define HTTP API data structures

--- src/models.rs
+++ src/models.rs
@@ -31,3 +31,118 @@ pub mod opentelemetry {
         }
     }
 }
+
+pub mod http {
+    use std::collections::HashMap;
+
+    use serde::Serialize;
+
+    #[derive(Serialize)]
+    #[serde(transparent)]
+    pub struct TraceId(String);
+
+    #[derive(Serialize)]
+    #[serde(transparent)]
+    pub struct SpanId(String);
+
+    #[derive(Serialize)]
+    #[serde(transparent)]
+    pub struct ProcessId(String);
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Trace {
+        #[serde(rename = "traceID")]
+        pub trace_id: TraceId,
+        pub spans: Vec<Span>,
+        pub processes: HashMap<String, Process>,
+        pub warnings: Vec<String>,
+    }
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Span {
+        #[serde(rename = "traceID")]
+        pub trace_id: TraceId,
+        #[serde(rename = "spanID")]
+        pub span_id: SpanId,
+        // deprecated
+        #[serde(rename = "parentSpanID", skip_serializing_if = "Option::is_none")]
+        pub parent_span_id: Option<SpanId>,
+        pub flags: u32,
+        pub operation_name: String,
+        pub references: Vec<Reference>,
+        pub start_time: u64,
+        pub duration: u64,
+        pub tags: Vec<KeyValue>,
+        pub logs: Vec<Log>,
+        #[serde(rename = "processID")]
+        pub process_id: ProcessId,
+        pub process: Option<Process>,
+        pub warnings: Vec<String>,
+    }
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Reference {
+        pub ref_type: ReferenceType,
+        #[serde(rename = "traceID")]
+        pub trace_id: TraceId,
+        #[serde(rename = "spanID")]
+        pub span_id: SpanId,
+    }
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+    pub enum ReferenceType {
+        ChildOf,
+        FollowsFrom,
+    }
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Process {
+        pub service_name: String,
+        pub tags: Vec<KeyValue>,
+    }
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Log {
+        pub timestamp: u64,
+        pub fields: Vec<KeyValue>,
+    }
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct KeyValue {
+        pub key: String,
+        #[serde(flatten)]
+        pub value: Value,
+    }
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "lowercase", tag = "type", content = "value")]
+    pub enum Value {
+        String(String),
+        Bool(bool),
+        Int64(i64),
+        Float64(f64),
+        Binary(Vec<u8>),
+    }
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct DependencyLink {
+        pub parent: String,
+        pub child: String,
+        pub call_count: u64,
+    }
+
+    #[derive(Serialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Operation {
+        pub name: String,
+        pub span_kind: String,
+    }
+}
