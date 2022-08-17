7e42ca7a8f2bcb19e83a1ecb276bfbd3944025f8
Dominik Nakamura <dnaka91@gmail.com>
2022-08-17 13:24:23 +0900

    Work around Thrift codegen issues

--- archer-thrift/src/lib.rs
+++ archer-thrift/src/lib.rs
@@ -1,5 +1,18 @@
+// rules copied from the Thrift codegen output
+#![allow(unused_imports)]
+#![allow(unused_extern_crates)]
+#![allow(clippy::too_many_arguments, clippy::type_complexity, clippy::vec_box)]
+
 pub use thrift;
 
-pub mod agent;
-pub mod jaeger;
-pub mod zipkincore;
+pub mod agent {
+    include!(concat!(env!("OUT_DIR"), "/agent.rs"));
+}
+
+pub mod jaeger {
+    include!(concat!(env!("OUT_DIR"), "/jaeger.rs"));
+}
+
+pub mod zipkincore {
+    include!(concat!(env!("OUT_DIR"), "/zipkincore.rs"));
+}
