f28c27fabc06627f5b852c628fb58d32931db3fb
Dominik Nakamura <dnaka91@gmail.com>
2022-08-17 11:21:22 +0900

    Adjust Protobuf/Thrift build scripts

--- archer-proto/build.rs
+++ archer-proto/build.rs
@@ -1,6 +1,9 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
+    println!("cargo:rerun-if-changed=../jaeger-idl/opentelemetry-proto/opentelemetry");
+    println!("cargo:rerun-if-changed=../jaeger-idl/proto");
+
     prost_build::compile_protos(
         &["../jaeger-idl/proto/api_v3/query_service.proto"],
         &[
--- archer-thrift/build.rs
+++ archer-thrift/build.rs
@@ -1,9 +1,19 @@
 fn main() {
+    println!("cargo:rerun-if-changed=../jaeger-idl/thrift");
+
+    let thrift = which::which("thrift").unwrap();
     let out_dir = std::env::var("OUT_DIR").unwrap();
-    std::process::Command::new("thrift")
+
+    std::process::Command::new(thrift)
         .arg("-out")
         .arg(out_dir)
-        .args(["--gen", "rs", "-r", "../jaeger-idl/thrift/jaeger.thrift"])
+        .args([
+            "--gen",
+            "rs",
+            "../jaeger-idl/thrift/agent.thrift",
+            "../jaeger-idl/thrift/jaeger.thrift",
+            "../jaeger-idl/thrift/zipkincore.thrift",
+        ])
         .output()
         .unwrap();
 }
