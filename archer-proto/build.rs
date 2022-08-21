66603a03c64ba5e53f9a7efba210f7e1b3603f75
Dominik Nakamura <dnaka91@gmail.com>
2022-08-21 16:28:52 +0900

    Don't generate unneeded Protobuf structures

--- archer-proto/build.rs
+++ archer-proto/build.rs
@@ -5,12 +5,10 @@ fn main() -> Result<()> {
         &[
             "../jaeger-idl/proto/api_v2/collector.proto",
             "../jaeger-idl/proto/api_v2/model.proto",
-            "../jaeger-idl/proto/api_v3/query_service.proto",
         ],
         &[
             "external",
             "../jaeger-idl/proto/api_v2",
-            "../jaeger-idl/proto/api_v3",
             "../jaeger-idl/opentelemetry-proto",
         ],
     )
