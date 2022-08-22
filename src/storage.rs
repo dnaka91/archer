1f5c1b69db977699ebd03f9fa336e8cad0b8804d
Dominik Nakamura <dnaka91@gmail.com>
2022-08-22 13:33:52 +0900

    Reduce boilerplate in SQLite calls

--- src/storage.rs
+++ src/storage.rs
@@ -57,56 +57,56 @@ fn async_fn(f: fn(&mut Connection) -> rusqlite::Result<()>) -> Hook<Manager> {
 }
 
 impl Database {
+    async fn interact<F, T>(&self, f: F) -> Result<T>
+    where
+        F: FnOnce(&mut Connection) -> Result<T, rusqlite::Error> + Send + 'static,
+        T: Send + 'static,
+    {
+        self.0
+            .get()
+            .await?
+            .interact(f)
+            .await
+            .map_err(|e| anyhow!("{e}"))?
+            .map_err(Into::into)
+    }
+
     pub async fn save_span(&self, span: Span) -> Result<()> {
         let buf = postcard::to_stdvec(&span)?;
         let buf = zstd::bulk::compress(&buf, 11)?;
 
-        self.0
-            .get()
-            .await?
-            .interact(move |conn| {
-                conn.execute(
-                    include_str!("queries/save_span.sql"),
-                    params![
-                        span.trace_id.to_be_bytes(),
-                        &span.process.service,
-                        &span.operation_name,
-                        buf,
-                    ],
-                )
-            })
-            .await
-            .map_err(|e| anyhow!("{e}"))??;
+        self.interact(move |conn| {
+            conn.execute(
+                include_str!("queries/save_span.sql"),
+                params![
+                    span.trace_id.to_be_bytes(),
+                    &span.process.service,
+                    &span.operation_name,
+                    buf,
+                ],
+            )
+        })
+        .await?;
 
         Ok(())
     }
 
     pub async fn list_services(&self) -> Result<Vec<String>> {
-        self.0
-            .get()
-            .await?
-            .interact(|conn| {
-                conn.prepare(include_str!("queries/list_services.sql"))?
-                    .query_map([], |row| row.get(0))?
-                    .collect::<Result<Vec<_>, _>>()
-            })
-            .await
-            .map_err(|e| anyhow!("{e}"))?
-            .map_err(Into::into)
+        self.interact(|conn| {
+            conn.prepare(include_str!("queries/list_services.sql"))?
+                .query_map([], |row| row.get(0))?
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .await
     }
 
     pub async fn list_operations(&self, service: String) -> Result<Vec<String>> {
-        self.0
-            .get()
-            .await?
-            .interact(|conn| {
-                conn.prepare(include_str!("queries/list_operations.sql"))?
-                    .query_map([service], |row| row.get(0))?
-                    .collect::<Result<Vec<_>, _>>()
-            })
-            .await
-            .map_err(|e| anyhow!("{e}"))?
-            .map_err(Into::into)
+        self.interact(|conn| {
+            conn.prepare(include_str!("queries/list_operations.sql"))?
+                .query_map([service], |row| row.get(0))?
+                .collect::<Result<Vec<_>, _>>()
+        })
+        .await
     }
 
     pub async fn list_spans(
@@ -115,16 +115,12 @@ impl Database {
         _operation: Option<String>,
     ) -> Result<Vec<Span>> {
         let spans = self
-            .0
-            .get()
-            .await?
             .interact(|conn| {
                 conn.prepare(include_str!("queries/list_spans.sql"))?
                     .query_map([service], |row| row.get::<_, Vec<u8>>(0))?
                     .collect::<Result<Vec<_>, _>>()
             })
-            .await
-            .map_err(|e| anyhow!("{e}"))??;
+            .await?;
 
         spans
             .into_iter()
@@ -138,16 +134,12 @@ impl Database {
 
     pub async fn find_trace(&self, trace_id: u128) -> Result<Vec<Span>> {
         let spans = self
-            .0
-            .get()
-            .await?
             .interact(move |conn| {
                 conn.prepare(include_str!("queries/find_trace.sql"))?
                     .query_map([trace_id.to_be_bytes()], |row| row.get::<_, Vec<u8>>(0))?
                     .collect::<Result<Vec<_>, _>>()
             })
-            .await
-            .map_err(|e| anyhow!("{e}"))??;
+            .await?;
 
         spans
             .into_iter()
