256384493814f945bc2d67a68ccc734ec6496d5e
Dominik Nakamura <dnaka91@gmail.com>
2022-09-05 05:27:36 +0000

    Load certificate from local storage (in examples)

--- tracing-quiver/examples/simple.rs
+++ tracing-quiver/examples/simple.rs
@@ -4,21 +4,13 @@ use anyhow::Result;
 use tracing::{debug_span, info, instrument, warn, Level};
 use tracing_subscriber::{filter::Targets, prelude::*};
 
-const CERTIFICATE: &str = "-----BEGIN CERTIFICATE-----
-MIIBWzCCAQCgAwIBAgIIOuisjYJ/sCwwCgYIKoZIzj0EAwIwITEfMB0GA1UEAwwW
-cmNnZW4gc2VsZiBzaWduZWQgY2VydDAgFw03NTAxMDEwMDAwMDBaGA80MDk2MDEw
-MTAwMDAwMFowITEfMB0GA1UEAwwWcmNnZW4gc2VsZiBzaWduZWQgY2VydDBZMBMG
-ByqGSM49AgEGCCqGSM49AwEHA0IABDQGRlxUCoK3AxFH+9dfmzda2ucydRpP2IXe
-yMX9mFt0p1njajXmKmpmV9JrNMjjhwxwv/oQEwdPud9a0ANYXsmjIDAeMBwGA1Ud
-EQQVMBOCCWxvY2FsaG9zdIIGYXJjaGVyMAoGCCqGSM49BAMCA0kAMEYCIQDkTIQv
-EeV0vsXSiwaorj0I/+Zk5j5W8dWtkk+2myqMlQIhAIovNRj0fdk6TrcLJfdyXPTa
-DlljIQNJ6cCTK33ar8fJ
------END CERTIFICATE-----";
-
 #[tokio::main]
 async fn main() -> Result<()> {
+    let certificate = concat!(env!("CARGO_MANIFEST_DIR"), "/../.local/data/quiver/cert.pem");
+    let certificate = tokio::fs::read_to_string(certificate).await?;
+
     let (quiver, handle) = tracing_quiver::builder()
-        .with_server_cert(CERTIFICATE)
+        .with_server_cert(certificate)
         .with_resource(env!("CARGO_CRATE_NAME"), env!("CARGO_PKG_VERSION"))
         .build()
         .await?;
